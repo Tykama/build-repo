03959095a79081cff69bda6463437106029178ab
chieftonyit <chieftonyit1985@gmail.com>
2025-01-30 22:25:42 -0600

    final

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "ec2-module" {
-  source = "git@github.com:Tykama/modules-repo.git//ec2?ref=v1.0.0"
+  source = "git@github.com:Tykama/modules-repo.git//ec2?ref=v1.1.0"
 
   inst_type = "t2.micro"
   name = "tonyit"
